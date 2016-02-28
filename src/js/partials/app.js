8eea03e907283eaa29160ae9681a2e810c9bea30
braun2002 <srg.tsygankov@gmail.com>
2016-02-28 17:09:25 +0300

    complete header responsive

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,3 +1,20 @@
 $(function(){
    //alert('its work');
+    //TODO:Дикий костыль, потом доделат!
+    $(".sel").hover(function(){
+        $(".other").css("opacity", "1");
+        $(".other").css("visibility", "visible");
+    }, function(){
+
+        $(".other").css("opacity", "0");
+        $(".other").css("visibility", "hidden");
+    });
+    $(".other").hover(function(){
+        $(".other").css("opacity", "1");
+        $(".other").css("visibility", "visible");
+    }, function(){
+
+        $(".other").css("opacity", "0");
+        $(".other").css("visibility", "hidden");
+    });
 });
